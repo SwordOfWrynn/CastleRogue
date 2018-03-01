2c003acd44e3f10ad4a7d5c1fd7f38dff6c362ac
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-01 13:55:09 -0800

    Stuff

---
+++ Castle Rogue/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject {
+
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+	// Use this for initialization
+	protected override void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
