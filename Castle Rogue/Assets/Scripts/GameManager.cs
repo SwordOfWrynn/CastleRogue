7b1f3dc26ef60264d358f5dc1cc475819c409290
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-15 11:44:35 -0700

    V 0.5 Ads and a updated icon

--- Castle Rogue/Assets/Scripts/GameManager.cs
+++ Castle Rogue/Assets/Scripts/GameManager.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.Advertisements;
-using UnityEditor;
 
 public class GameManager : MonoBehaviour {
 
