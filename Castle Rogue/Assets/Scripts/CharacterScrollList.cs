e961961477bda1ba81f589c50539d1801652e517
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-16 13:10:17 -0700

    6:14 video 05

---
+++ Castle Rogue/Assets/Scripts/CharacterScrollList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite itemImage;
+    public string itemDescription;
+    public int price = 1;
+}
+
+public class CharacterScrollList : MonoBehaviour {
+
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public CharacterScrollList otherScrollList;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Castle Rogue/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour {
+
+    public GameObject characterInfo;
+    public Button purchaseOrSelect;
+    public Text buttonText;
+    public Text characterName;
+    public Text characterDescription;
+    public Image characterImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetUp()
+    {
+
+    }
+}
