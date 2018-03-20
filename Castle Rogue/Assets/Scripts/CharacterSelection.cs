253601acda0e471f754213e36ca45636c1ba73bd
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-20 13:52:08 -0700

    Foo

--- Castle Rogue/Assets/Scripts/CharacterSelection.cs
+++ Castle Rogue/Assets/Scripts/CharacterSelection.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour {
 
-    //base right
+    //right character info
     public GameObject characterInfo;
     public Button purchaseOrSelect;
     public Text buttonText;
     public Text characterName;
     public Text characterDescription;
-    public Image characterImage;
-
+    public GameObject characterImage;
     //left buttons
+    public Button thisButton;
     public Sprite newSprite;
-    public Text newCharacterName;
-    public Text newCharacterDescription;
+    public string newCharacterName;
+    public string newCharacterDescription;
+
+    private bool femmeFatale = false;
 
     // Use this for initialization
     void Start () {
@@ -28,8 +30,17 @@ public class CharacterSelection : MonoBehaviour {
     }
     public void OnClick()
     {
-        characterName = newCharacterName;
-        characterDescription = newCharacterDescription;
-
+        characterName.text = newCharacterName;
+        characterDescription.text = newCharacterDescription;
+        characterImage.GetComponent<Image>().sprite = newSprite;
     }
+
+    public void SelectOrPurchase()
+    {
+        if (femmeFatale)
+        {
+
+        }
+    }
+
 }
