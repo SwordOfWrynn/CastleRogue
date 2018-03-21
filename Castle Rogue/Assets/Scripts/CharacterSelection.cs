dcb2db57bbb625f7047787d3f2f9a41db5d6ad2f
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-21 11:38:20 -0700

    Foo

--- Castle Rogue/Assets/Scripts/CharacterSelection.cs
+++ Castle Rogue/Assets/Scripts/CharacterSelection.cs
@@ -5,24 +5,30 @@ using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour {
 
+    public int money = 50000;
+
     //right character info
     public GameObject characterInfo;
-    public Button purchaseOrSelect;
-    public Text buttonText;
+    public GameObject selectButton;
+    public GameObject purchaseButton;
+    public Text purchaseButtonText;
     public Text characterName;
     public Text characterDescription;
     public GameObject characterImage;
     //left buttons
+    public int price;
     public Button thisButton;
     public Sprite newSprite;
     public string newCharacterName;
     public string newCharacterDescription;
+    public Button[] allButtons;
 
+    private bool rogue = true;
     private bool femmeFatale = false;
 
     // Use this for initialization
     void Start () {
-		
+        selectButton.SetActive(false);
 	}
     public void SetUp()
     {
@@ -30,16 +36,34 @@ public class CharacterSelection : MonoBehaviour {
     }
     public void OnClick()
     {
+        purchaseButtonText.text = ("Price" + price);
         characterName.text = newCharacterName;
         characterDescription.text = newCharacterDescription;
         characterImage.GetComponent<Image>().sprite = newSprite;
+        if (thisButton == allButtons[0])
+        {
+            if (rogue == true)
+            {
+                selectButton.SetActive(true);
+                purchaseButton.SetActive(false);
+            }
+        }
+        if (thisButton == allButtons[1])
+        {
+            if (femmeFatale == true)
+            {
+                selectButton.SetActive(true);
+                purchaseButton.SetActive(false);
+            }
+        }
     }
 
-    public void SelectOrPurchase()
+    public void Purchase()
     {
-        if (femmeFatale)
+        if (money >= price)
         {
-
+            money = money - price;
+            femmeFatale = true;
         }
     }
 
