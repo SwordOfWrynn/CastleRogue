3a8756f32e1e731b1d09455c27b9d2bb6e8416bd
SwordOfWrynn <bubba.wiens@gmail.com>
2018-03-26 13:59:58 -0700

    stuff

--- Castle Rogue/Assets/Scripts/CreateSelectedPlayer.cs
+++ Castle Rogue/Assets/Scripts/CreateSelectedPlayer.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class CreateSelectedPlayer : MonoBehaviour {
 
-    private int selectedPlayer;
+    private int character;
 
+    public GameObject[] characterPrefabs;
 	// Use this for initialization
 	void Start () {
         if (File.Exists(Application.persistentDataPath + "/SelectedCharacter.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/SelectedCharacter.dat", FileMode.Open);
-            SelectInfo myLoadedInfo = (UnlockInfo)bf.Deserialize(file);
-            selectedPlayer = myLoadedInfo.character;
+            SelectInfo myLoadedInfo = (SelectInfo)bf.Deserialize(file);
+            character = myLoadedInfo.character;
         }
+        else
+            character = 0;
+        Instantiate(characterPrefabs[character], new Vector3(0f, 0f, 0f), Quaternion.identity);
     }
 	
 	// Update is called once per frame
@@ -23,6 +29,6 @@ public class CreateSelectedPlayer : MonoBehaviour {
 	}
     public class SelectInfo
     {
-        public int selectedPlayer;
+        public int character;
     }
 }
