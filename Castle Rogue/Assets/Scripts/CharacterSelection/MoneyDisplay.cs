217f64ff40f2bd6e5b72f9b105d508e38bac1bdc
SwordOfWrynn <bubba.wiens@Gmail.com>
2018-04-18 11:49:27 -0700

    Foo

---
+++ Castle Rogue/Assets/Scripts/CharacterSelection/MoneyDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplay : MonoBehaviour {
+
+    private CurrencyManager currencyManager;
+    private Text moneyText;
+
+	// Use this for initialization
+	void Start () {
+        currencyManager = GameObject.Find("CurrencyManager").GetComponent<CurrencyManager>();
+        moneyText = GetComponent<Text>();
+        moneyText.text = currencyManager.money.ToString();
+	}
+	public void RefreshMoney()
+    {
+        moneyText.text = "Money: " + currencyManager.money.ToString();
+    }
+}
